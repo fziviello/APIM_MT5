e48e755ea86356869549f8398ed913a09d09a68e
Fziviello <f.ziviello@email.it>
2024-11-25 16:03:25 +0100

    updated

--- server.py
+++ server.py
@@ -25,8 +25,20 @@ if account_info:
 else:
     logging.info(f"Errore account:{mt5.last_error()}")
 
+def get_order():
+    orders = mt5.orders_get()
+
+    if orders is None or len(orders) == 0:
+        message = f"Non esistono ordini: {mt5.last_error()}"
+        logging.error(message)
+        return {"success": True, "message": message, "orders": []}
+
+    return {"success": True, "orders": orders}
 
 def create_order(symbol, order_type, volume, price=None, sl=None, tp=None, magic=0):
+
+    infoSymbol = mt5.symbol_info(symbol)
+
     order_type_mapping = {
         'buy': mt5.ORDER_TYPE_BUY,
         'sell': mt5.ORDER_TYPE_SELL,
@@ -67,22 +79,33 @@ def create_order(symbol, order_type, volume, price=None, sl=None, tp=None, magic
         logging.info(f"Prezzo calcolato automaticamente per {order_type}: {price}")
 
     if order_type in ['buy', 'sell']:
-        tp, sl = None, None
-
-    request = {
-        "action": mt5.TRADE_ACTION_DEAL if order_type in ['buy', 'sell'] else mt5.TRADE_ACTION_PENDING,
-        "symbol": symbol,
-        "volume": volume,
-        "type": order_type_mapping[order_type],
-        "price": price,
-        "sl": sl,
-        "tp": tp,
-        "deviation": 10,
-        "magic": magic,
-        "type_time": mt5.ORDER_TIME_GTC,
-        "type_filling": mt5.ORDER_FILLING_RETURN,
-        "comment": DESCRIPTION
-    }
+        request = {
+                "action": mt5.TRADE_ACTION_DEAL,
+                "symbol": symbol,
+                "volume": volume,
+                "type": order_type_mapping[order_type],
+                "price": price,
+                "deviation": 10,
+                "magic": magic,
+                "type_time": mt5.ORDER_TIME_GTC,
+                "type_filling":mt5.ORDER_FILLING_IOC,
+                "comment": DESCRIPTION
+            }
+    else:
+        request = {
+            "action": mt5.TRADE_ACTION_DEAL if order_type in ['buy', 'sell'] else mt5.TRADE_ACTION_PENDING,
+            "symbol": symbol,
+            "volume": volume,
+            "type": order_type_mapping[order_type],
+            "price": price,
+            "sl": sl,
+            "tp": tp,
+            "deviation": 10,
+            "magic": magic,
+            "type_time": mt5.ORDER_TIME_GTC,
+            "type_filling": mt5.ORDER_FILLING_RETURN,
+            "comment": DESCRIPTION
+        }
 
     result = mt5.order_send(request)
     if result is None:
@@ -100,68 +123,59 @@ def create_order(symbol, order_type, volume, price=None, sl=None, tp=None, magic
     return {"success": True, "order": result.order}
 
 def update_order(ticket, stop_loss=None, take_profit=None):
+
     orders = mt5.orders_get(ticket=ticket)
     if not orders:
-        message = f"Ordine con ticket {ticket} non trovato."
+        message = f"Nessun ordine pendente trovato: {mt5.last_error()}"
         logging.error(message)
         return {"success": False, "message": message}
 
-    order = orders[0]
-    
-    new_order = create_order(
-        symbol=order.symbol,
-        order_type=order.type,
-        volume=order.volume,
-        price=order.price,
-        sl=stop_loss if stop_loss else order.sl,
-        tp=take_profit if take_profit else order.tp,
-        magic=order.magic
-    )
-    
-    if new_order["success"]:
-        delete_result = delete_order(ticket)
-        return {"success": True, "message": "Ordine aggiornato con successo"}
-    else:
-        return {"success": False, "message": "Errore nell'aggiornamento dell'ordine"}
-
-def delete_order(ticket):
-    orders = mt5.orders_get(ticket=ticket)
-    
-    if not orders:
-        message = f"Ordine con ticket {ticket} non trovato."
-        logging.error(message)
-        return {"success": False, "message": message}
-    
-    order = orders[0]
-    
-    if order.type not in [mt5.ORDER_TYPE_BUY_LIMIT, mt5.ORDER_TYPE_SELL_LIMIT, mt5.ORDER_TYPE_BUY_STOP, mt5.ORDER_TYPE_SELL_STOP]:
-        message = f"Impossibile annullare l'ordine con ticket {ticket}: solo ordini pendenti possono essere cancellati."
-        logging.error(message)
-        return {"success": False, "message": message}
-
-    logging.info(f"Dettagli ordine da cancellare: {order}")
-    
     request = {
-        "action": mt5.TRADE_ACTION_REMOVE,
-        "symbol": order.symbol,
-        "type": order.type,
-        "ticket": ticket,
-        "deviation": 10,
-        "magic": order.magic,
-        "type_time": mt5.ORDER_TIME_GTC,
-        "type_filling": mt5.ORDER_FILLING_RETURN,
+        "action": mt5.TRADE_ACTION_SLTP,
+        "order": ticket,
+        "sl": stop_loss,
+        "tp": take_profit,
     }
-    
+
     result = mt5.order_send(request)
     if result is None:
-        message = mt5.last_error()
+        message = f"Errore di comunicazione con il server: {mt5.last_error()}"
         logging.error(message)
         return {"success": False, "message": message}
 
     if result.retcode != mt5.TRADE_RETCODE_DONE:
-        message = f"Errore nell'annullamento dell'ordine: {result.comment}"
+        message = f"Errore nell'aggiornamento dell'ordine. Retcode: {result.retcode}, Commento: {result.comment}"
         logging.error(message)
         return {"success": False, "message": message}
+
+    logging.info(f"Ordine con ticket {ticket} aggiornato con successo.")
+    return {"success": True, "message": "Ordine aggiornato con successo"}
+
+def delete_order(ticket):      
+    orders = mt5.orders_get(ticket=ticket)
+   
+    if not orders:
+        message = f"Nessun ordine trovato o errore: {mt5.last_error()}"
+        logging.error(message)
+        return {"success": False, "message": message}
+
+    logging.info(f"Ordine {ticket} da eliminare")
+    
+    request = {
+        "action": mt5.TRADE_ACTION_REMOVE,
+        "order": ticket,
+    }
+    
+    result = mt5.order_send(request)
+    if result is None:
+        error_message = f"Errore di comunicazione con il server: {mt5.last_error()}"
+        logging.error(error_message)
+        return {"success": False, "message": error_message}
+
+    if result.retcode != mt5.TRADE_RETCODE_DONE:
+        error_message = f"Errore nell'annullamento dell'ordine. Retcode: {result.retcode}, Commento: {result.comment}"
+        logging.error(error_message)
+        return {"success": False, "message": error_message}
 
     logging.info(f"Ordine con ticket {ticket} cancellato con successo.")
     return {"success": True, "message": "Ordine cancellato con successo"}
@@ -169,6 +183,9 @@ def delete_order(ticket):
 @app.route('/order', methods=['POST'])
 def create_order_api():
     try:
+        if not mt5.initialize():
+            return jsonify({"success": False, "message": f"Errore inizializzazione MT5: {mt5.last_error()}"}), 500
+        
         data = request.get_json()
         symbol = data['symbol']
         order_type = data['type']
@@ -190,9 +207,12 @@ def create_order_api():
         logging.exception("Errore nella creazione dell'ordine")
         return jsonify({"status": "error", "message": str(e)}), 400
 
-@app.route('/order/update', methods=['POST'])
+@app.route('/order/update', methods=['PUT'])
 def update_order_api():
     try:
+        if not mt5.initialize():
+            return jsonify({"success": False, "message": f"Errore inizializzazione MT5: {mt5.last_error()}"}), 500
+        
         data = request.get_json()
         order_ticket = data['ticket']
         take_profit = data.get('take_profit')
@@ -210,6 +230,9 @@ def update_order_api():
 @app.route('/order/delete', methods=['DELETE'])
 def delete_order_api():
     try:
+        if not mt5.initialize():
+            return jsonify({"success": False, "message": f"Errore inizializzazione MT5: {mt5.last_error()}"}), 500
+        
         data = request.get_json()
         order_ticket = data['ticket']
         
@@ -220,6 +243,19 @@ def delete_order_api():
             return jsonify({"status": "error", "message": delete_result["message"]}), 400
     except Exception as e:
         logging.exception("Errore nella cancellazione dell'ordine")
+        return jsonify({"status": "error", "message": str(e)}), 400
+
+@app.route('/order', methods=['GET'])
+def get_order_api():
+    try:
+        get_order_result = get_order()
+       
+        if get_order_result["success"]:
+            return jsonify({"status": "success", "orders": get_order_result["orders"]}), 200
+        else:
+            return jsonify({"status": "error", "message":  get_order_result["message"]}), 400
+    except Exception as e:
+        logging.exception("Errore nella ricezione della lista degli ordini")
         return jsonify({"status": "error", "message": str(e)}), 400
 
 if __name__ == '__main__':
